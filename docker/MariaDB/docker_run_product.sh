b5bc95c41c52c09ab20f471de37ac68427e7be8d
Zhang He <zhang_he06@163.com>
2019-12-05 11:53:51 +0800

    Update MariaDB Docker

--- docker/MariaDB/docker_run_product.sh
+++ docker/MariaDB/docker_run_product.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-DB_DIR=${PROJECT_PATH}'/docker/MariaDB/'
+DB_DIR=${PROJECT_PATH}'/docker/MariaDB'
 
-[ -d ${DB_DIR}data ] || mkdir -p ${DB_DIR}data
-[ -d ${DB_DIR}log ] || mkdir -p ${DB_DIR}log
+[ -d "${DB_DIR}"/data ] || mkdir -p "${DB_DIR}"/data
+[ -d "${DB_DIR}"/log ] || mkdir -p "${DB_DIR}"/log
 
 docker run \
     -h mariadb \
     --name mariadb \
-    -v ${DB_DIR}data:/var/lib/mysql \
-    -v ${DB_DIR}log:/var/log/mysql \
+    -v "${DB_DIR}"/data:/var/lib/mysql \
+    -v "${DB_DIR}"/log:/var/log/mysql \
     -e MYSQL_ROOT_PASSWORD='123456' \
     -e MYSQL_DATABASE='bearing_project' \
     -e MYSQL_USER='www' \
