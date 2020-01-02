f01e70dd9b80671886efa9e38e45ba92ac90d41a
Zhang He <zhang_he06@163.com>
2020-01-02 13:20:18 +0800

    Update ExporterNode Docker

--- docker/ExporterNode/docker_run.sh
+++ docker/ExporterNode/docker_run.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
 docker run \
+    -h node-exporter \
     --name node-exporter \
+    --pid "host" \
+    --cpus ".25" \
+    --memory "500m" \
+    --memory-swap "500m" \
+    -v "/:/host:ro" \
+    -p 9100:9100 \
     -d \
-    --net="host" \
-    --pid="host" \
-    -v "/:/host:ro,rslave" \
-    prom/node-exporter \
+    prom/node-exporter:v0.18.1 \
     --path.rootfs /host
+
+# --net="host" 在容器内使用主机的网络命名空间（相比-p 9100:9100没有网络转发）
+# --pid="host" 在容器内使用主机的进程进程空间
+
+#docker run \
+#    -d --name node-exporter \
+#    -v "/proc:/host/proc" \
+#    -v "/sys:/host/sys" \
+#    -v "/:/rootfs" \
+#    --net="host" \
+#    prom/node-exporter:v0.18.1 \
+#    --path.procfs=/host/proc \
+#    --path.sysfs=/host/proc \
+#    --collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)"
--- docker/ExporterNode/docker_run_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-    --name node-exporter \
-    -d \
-    -p 9100:9100 \
-    prom/node-exporter
