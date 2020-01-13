ff94b23a7a940783d6a9e069ee1106a8a481a9d5
Zhang He <zhang_he06@163.com>
2020-01-13 13:34:37 +0800

    Update Prometheus Docker

--- docker/Prometheus/docker_run.sh
+++ docker/Prometheus/docker_run.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-[ -d ${PWD}/conf ] || mkdir -p ${PWD}/conf
+# Create Docker Volume
+VOLUME_NAME="prometheus-vol"
+
+# docker volume rm prometheus-vol
+
+docker volume ls | grep -wq "$VOLUME_NAME" && \
+    echo "The volume: $VOLUME_NAME already exists" || (docker volume create "$VOLUME_NAME" && \
+    echo "The volume: $VOLUME_NAME has been created")
 
 docker run \
+    -h prometheus \
     --name prometheus \
-    -d \
-    -p 9090:9090 \
+    --cpus ".25" \
+    --memory "1g" \
+    --memory-swap "1g" \
     -v ${PWD}/conf/prometheus.yml:/etc/prometheus/prometheus.yml \
-    prom/prometheus
+    -v "$VOLUME_NAME":/prometheus \
+    -p 9090:9090 \
+    -d \
+    prom/prometheus:v2.16.0
+
+# --net="host" 在容器内使用主机的网络命名空间（相比 -p 9090:9090 没有网络转发）
