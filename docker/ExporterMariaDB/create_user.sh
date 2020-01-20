322f7389119071e356e9c1b933246cdaa81e78c7
Zhang He <zhang_he06@163.com>
2020-01-20 13:41:07 +0800

    Update ExporterMariaDB Docker

---
+++ docker/ExporterMariaDB/create_user.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker run \
+    -it \
+    --link mariadb:mysql \
+    --rm \
+    mariadb:10.1.23 \
+    sh -c 'exec mysql \
+    -h"$MYSQL_PORT_3306_TCP_ADDR" \
+    -P"$MYSQL_PORT_3306_TCP_PORT" \
+    -uroot \
+    -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" \
+    --default-character-set=utf8 \
+    "$MYSQL_ENV_MYSQL_DATABASE" \
+    -e "CREATE USER 'exporter'@'localhost' IDENTIFIED BY '\'123456\'';GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'localhost';GRANT SELECT ON performance_schema.* TO 'exporter'@'localhost';FLUSH PRIVILEGES;select host,user from mysql.user;"'
