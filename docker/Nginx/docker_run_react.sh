3f31a0c24738e8fd850590fbc8fb9dfc62abc354
Zhang He <zhang_he06@163.com>
2020-02-03 14:07:48 +0800

    Update Nginx React Docker

--- docker/Nginx/docker_run_react.sh
+++ docker/Nginx/docker_run_react.sh
@@ -6,9 +6,9 @@ docker run \
     -v ${PWD}/conf/nginx_react.conf:/etc/nginx/nginx.conf:ro \
     -v ${PWD}/conf/conf_react.d:/etc/nginx/conf.d:ro \
     -v ${PWD}/logs:/var/log/nginx \
-    -v ${PWD}/dist:/usr/share/nginx/dist \
+    -v ${PWD}/dist_react:/usr/share/nginx/dist \
     -p 8500:8500 \
     -d \
-    nginx:1.13.0
+    nginx:logrotate
 
 # 注意这里挂载设置了:ro只读，宿主机更新文件之后，不会更新到docker内，需要删除容器，重开容器
