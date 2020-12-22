724ca6911db3e3cbe5e1de35305d35f531889133
Zhang He <zhang_he06@163.com>
2020-12-23 02:09:27 +0800

    Update resize

--- app_backend/static/js/common/panel_resize.js
+++ app_backend/static/js/common/panel_resize.js
@@ -2,12 +2,12 @@ $(function () {
     var collapse_search_obj = $('.panel-collapse')
 
     collapse_search_obj.on('shown.bs.collapse', function () {
-        $(this).parent().find('.panel_resize_btn').removeClass('glyphicon-resize-full')
-        $(this).parent().find('.panel_resize_btn').addClass('glyphicon-resize-small')
+        $(this).parent().find('.panel_resize_btn').removeClass('glyphicon-chevron-down')
+        $(this).parent().find('.panel_resize_btn').addClass('glyphicon-chevron-up')
     })
 
     collapse_search_obj.on('hidden.bs.collapse', function () {
-        $(this).parent().find('.panel_resize_btn').removeClass('glyphicon-resize-small')
-        $(this).parent().find('.panel_resize_btn').addClass('glyphicon-resize-full')
+        $(this).parent().find('.panel_resize_btn').removeClass('glyphicon-chevron-up')
+        $(this).parent().find('.panel_resize_btn').addClass('glyphicon-chevron-down')
     })
 })
